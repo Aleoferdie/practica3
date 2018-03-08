a3b6cb7b19453301125e79e2195eb25dc8f3fda0
Alejandro Orbaiceta <aleorbaiceta@gmail.com>
2018-03-08 19:29:53 +0100

    --all

--- app.js
+++ app.js
@@ -1,88 +1,48 @@
-
-var http = require('http');
+var express = require('express');
+var app = express();
 var path = require('path');
+var http = require('http');
 var fs = require('fs');
 var form = require('fs').readFileSync('content/contact.html');
 var util = require('util');
 var querystring = require('querystring');
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 var maxData = 20 * 1024 * 1024; // 20 MB
-var app = require('express');
+var pug = require('pug');
 
-var mimeTypes = {
-				'.js' : 'text/javascript',
-				'.html' : 'text/html',
-				'.css' : 'text/css',
-				'.png': 'image/png',
-				'.jpg': 'image/jpg'
-				};
+app.set('view engine', 'pug');
 
-var pags = [
-			{ruta: '/', output: 'index.html'},
-			{ruta: '/index', output: 'index.html'},
-			{ruta: '/about', output: 'acerca.html'},
-			{ruta: '/products', output: 'productos.html'},
-			{ruta: '/webservices', output: 'webserv.html'},
-			{ruta: '/contact', output: 'contact.html'},
-			{ruta: '/confirm', output: 'confirm.html'}
-		   ];				
+app.use(express.static('content')); // busca contenido en la carpeta especificada
 
-http.createServer(function(request, response) {
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/index.html')); //__dirname obtiene el path actual
+});
 
-	var ruta = decodeURI(request.url);
-	//console.log(ruta);
-	var buscar = path.basename(ruta) || 'index.html';
+app.get('/index', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/index.html'));
+});
 
-	pags.forEach(function(pag) {
+app.get('/about', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/about.html'));
+});
 
-		if (ruta == pag.ruta) {
-			buscar = pag.output;
-			return;
-		}
+app.get('/products', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/products.html'));
+});
 
-	});
+app.get('/contact', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/contact.html'));
+});
 
-	var f = 'content/' + buscar;
-	//console.log(f);
+app.get('/webservices', function(request, response) {
+	response.sendFile(path.join(__dirname + '/content/webserv.html'));
+});
 
-	fs.exists(f, function(exists) {
-		if (exists) {
-			fs.readFile(f, function(err, data) {
-				if (err) {
-					response.writeHead(500);
-					response.end('Error del servidor');
-					return;
-				}
-				var headers = {'Content-type': mimeTypes[path.extname(buscar)]};
-				response.writeHead(200, headers);
-				response.end(data);
-				if (request.method === 'POST') {
-					var postData = '';
-					request.on('data', function(chunk) {
-						postData += chunk;
-						if (postData.length > maxData) {
-							postData = '';
-							this.pause();
-							response.writeHead(413);
-							response.end('El mensaje es demasiado largo');
-						}
-					}).on('end', function () {
-						if (!postData) {
-							response.end();
-							return;
-					}
-						var postDataObject = querystring.parse(postData);
-						//console.log('Ha escrito:\n' + util.inspect(postDataObject));
-						response.end('Ha escrito:\n' + util.inspect(postDataObject))
-						});
-					return;
-				}
-			});
-			return;
-		}
-		response.writeHead(404);
-		response.end('Pagina no encontrada');
-	});
-
-}).listen(8080);
+app.post('/confirm', parseUrlencoded, function(request, response) {
+	
+})
 
 console.log('Servidor escuchando en el puerto 8080');
+
+app.listen(8080);
